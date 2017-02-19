29ea44a910ffa2f86783b7593fc5a41a6d65e564
Oleg de Leon <oleg.deleon@gmail.com>
2017-02-20 01:58:34 +0800

    Adds JS / animate.css stuff

--- assets/js/springboard.js
+++ assets/js/springboard.js
@@ -1,1 +1,24 @@
+var formGroup = document.getElementsByClassName('form-container');
+var button = document.querySelector('.tabs');
 
+button.addEventListener("click", function(){
+  for(var i = 0; i < formGroup.length; i++){
+    formGroup[i].classList.toggle('hidden');
+    formGroup[i].classList.add('animated');
+    formGroup[i].classList.add('fadeIn');
+  }
+});
+
+// function toggleTabs(){
+//   for(var i = 0; i < button.length; i++){
+//     addEventListener(button[i], 'click', function(){
+//       for(var i = 0; i < formGroup.length; i++){
+//         formGroup[i].classList.toggle('hidden');
+//         formGroup[i].classList.add('animated');
+//         formGroup[i].classList.add('fadeIn');
+//       }
+//     });
+//   }
+// }
+//
+// toggleTabs();
