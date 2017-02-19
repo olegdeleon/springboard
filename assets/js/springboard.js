8984d5338762f0b11193ec037a96f79f6af82591
Oleg de Leon <oleg.deleon@gmail.com>
2017-02-20 02:46:06 +0800

    Adds jQuery

--- assets/js/springboard.js
+++ assets/js/springboard.js
@@ -1,6 +1,7 @@
 var formGroup = document.getElementsByClassName('form-container');
 var button = document.querySelector('.tabs');
 
+
 button.addEventListener("click", function(){
   for(var i = 0; i < formGroup.length; i++){
     formGroup[i].classList.toggle('hidden');
@@ -8,17 +9,3 @@ button.addEventListener("click", function(){
     formGroup[i].classList.add('fadeIn');
   }
 });
-
-// function toggleTabs(){
-//   for(var i = 0; i < button.length; i++){
-//     addEventListener(button[i], 'click', function(){
-//       for(var i = 0; i < formGroup.length; i++){
-//         formGroup[i].classList.toggle('hidden');
-//         formGroup[i].classList.add('animated');
-//         formGroup[i].classList.add('fadeIn');
-//       }
-//     });
-//   }
-// }
-//
-// toggleTabs();
